1fa869b215f16d3eb3efbf45affa422c50919cb9
UshasriKanagala <ushasrikanagala@gmail.com>
2025-07-29 20:14:05 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,24 @@
+package Lab1;
+
+public class Swapping {
+	public static void main(String[]args) {
+		int tom=3;
+		int jerry=5;
+		System.out.println("Tom has" + tom + "apples");
+		System.out.println("Jerry has" + jerry + "apples");
+		int swap = tom;
+		tom = jerry;
+		jerry = swap;
+		System.out.println("\nAfter this:");
+		System.out.println("Tom has" + tom + "apples");
+		System.out.println("Jerry has" + jerry + "apples");
+		
+		 
+	}
+	
+	
+	 
+
+
+
+}
